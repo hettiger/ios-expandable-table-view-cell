b8c53da3598f36a6a48debd7d8299e5386f7697c
Martin Hettiger <martin@hettiger.com>
2020-01-01 13:34:51 +0100

    Initial Commit

---
+++ Expandable Table View Cell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expandable Table View Cell
+//
+//  Created by Martin Hettiger on 01.01.20.
+//  Copyright © 2020 Hettiger IT-Dienstleistungen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
